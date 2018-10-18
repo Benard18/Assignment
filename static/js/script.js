ce6d24b919e96cfb8d876bdd68eafcb37b7febe4
Benard18 <benkaranja43@gmail.com>
2018-10-18 22:14:48 +0300

    add the changes

---
+++ static/js/script.js
@@ -0,0 +1,41 @@
+function myFunction() {
+  // Get the checkbox
+  var checkBox = document.getElementById("myCheck");
+  // Get the output text
+  var text = document.getElementById("text");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+function myFunctionLadies() {
+  // Get the checkbox
+  var checkBox = document.getElementById("test6");
+  // Get the output text
+  var text = document.getElementById("text2");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+function myFunctionChildren() {
+  // Get the checkbox
+  var checkBox = document.getElementById("test7");
+  // Get the output text
+  var text = document.getElementById("text3");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
